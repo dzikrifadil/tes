a23cf6355f3e2e14afaf39cc2380fca7f0432be8
dzikrifadil <55268608+dzikrifadil@users.noreply.github.com>
2021-12-19 16:11:15 +0700

    Update index.php

--- index.php
+++ index.php
@@ -10,6 +10,23 @@
  <title>Niagahoster Tutorial</title>
 </head>
 <body>
- ABCDEFG
+ <div class="alert alert-warning" role="alert">
+  <?php echo $_SESSION['error']?>
+ </div>
+
+ <div class="container">
+  <form action="" method="POST" class="login-email">
+   <p class="login-text" style="font-size: 2rem; font-weight: 800;">Login</p>
+   <div class="input-group">
+    <input type="text" placeholder="Username" name="email" value="<?php echo $email; ?>" required>
+   </div>
+   <div class="input-group">
+    <input type="password" placeholder="Password" name="password" value="<?php echo $_POST['password']; ?>" required>
+   </div>
+   <div class="input-group">
+    <button name="submit" class="btn">Login</button>
+   </div>
+  </form>
+ </div>
 </body>
 </html>
